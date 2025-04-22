54457b3b6cafcccc4eb32c11accf52adaddf8ae1
CodeMasterYuriy <yuriysatoshi@gmail.com>
2025-04-22 05:44:46 -0700

    fix: protect router on frontend

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -6,17 +6,12 @@ const PrivateRoute = () => {
   const store = useStore();
 
   useEffect(() => {
-    const state = useStore.getState();
-    if (!state.hydrated) {
-      state.getUser(); // try to load user from localStorage token
+    if (!store.hydrated) {
+      store.getUser(); // try to load user from localStorage token
     }
   }, []);
 
-  if (!store.hydrated) {
-    return null;  // Or a loading spinner until the state is hydrated
-  }
-
-  return store.authUser ? <Outlet /> : <Navigate to="/signin" replace />;
+  return store.hydrated ? <Outlet /> : <Navigate to="/signin" replace />;
 };
 
 export default PrivateRoute;
