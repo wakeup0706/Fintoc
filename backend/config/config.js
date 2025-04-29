0b94fa351718348816cfad161284f70f00260d46
CodeMasterYuriy <yuriysatoshi@gmail.com>
2025-04-29 07:52:54 -0700

    update: vercel,json

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: "root",
-    password: "",
-    database: "fintoc",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql' // directly hardcoded if process.env fails
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
   }
 };
