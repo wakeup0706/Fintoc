413f9ca168548a3512af3171cfe360ec3031e722
CodeMasterYuriy <yuriysatoshi@gmail.com>
2025-04-29 08:41:33 -0700

    fix: corrsss

--- backend/app.js
+++ backend/app.js
@@ -8,27 +8,30 @@ require('dotenv').config();
 
 const db = require('./models');
 const authRoutes = require('./routes/auth.routes');
+const fintocFoutes = require('./routes/fintoc.routes');
 const googleRoutes = require('./routes/google.routes');
 
 require('./config/google');
 require('./config/jwt');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// CORS Configuration
+const whitelist = ["http://localhost:3000"]
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production' ? 'https://fintoc.vercel.app' : 'http://localhost:3000', // Adjust to production URL
-  credentials: true, // Allow cookies to be sent
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these methods
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // Specify allowed headers
-  preflightContinue: false, // Ensure preflight requests are handled by Express
-  optionsSuccessStatus: 204, // Handle legacy browsers' issues
-};
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
 
-// Apply CORS middleware globally
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-// Body Parsing Middleware
+
 app.use(cookieSession({
   name: 'authSession',
   keys: [process.env.SESSION_KEY || 'secret'],
@@ -40,19 +43,13 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Route handlers
 app.use('/auth', authRoutes);
+app.use('/fintoc', fintocFoutes);
 app.use('/auth/google', googleRoutes);
 
-// Database connection and server startup
 db.sequelize.authenticate()
   .then(() => {
     console.log('Database connected');
-    app.listen(process.env.PORT || 5000, () => {
-      console.log('Server is running');
-    });
+    app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
   })
   .catch(err => console.error('DB connection failed:', err));
-
-// Handle preflight (OPTIONS) requests
-app.options('*', cors());
