02feee98165d8f65b5cf63fc2dce419804a1332b
CodeMasterYuriy <yuriysatoshi@gmail.com>
2025-04-30 07:25:44 -0700

    fixed: online server cors error

--- backend/api/app.js
+++ backend/api/app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS Configuration
 const corsOptions = {
-  origin: 'http://localhost:3000', // frontend URL
+  origin: 'https://fintoc.onrender.com', // frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
