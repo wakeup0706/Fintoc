3ad01559d20e834ab63140a110ca456a6215a93c
CodeMasterYuriy <yuriysatoshi@gmail.com>
2025-05-05 13:36:35 -0700

    fixed: google login

--- backend/api/config/google.js
+++ backend/api/config/google.js
@@ -23,7 +23,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const defaultRole = await Role.findOne({ where: { name: 'user' } });
